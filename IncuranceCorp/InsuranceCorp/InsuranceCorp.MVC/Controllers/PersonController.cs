e90550517165a66d9de58b6724230c212eb4b528
kubicek-skoleni <kubicek.brno@gmail.com>
2023-04-03 16:30:43 +0200

    Person controller detail

--- IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/PersonController.cs
+++ IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 ﻿using InsuranceCorp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InsuranceCorp.MVC.Controllers
 {
@@ -15,11 +16,22 @@ namespace InsuranceCorp.MVC.Controllers
         {
             // 1. ziskat data
             var top100 = _context.Persons
+                    .Include(person => person.Constracts)
                     .OrderBy(person => person.Id)
+                    //.OrderByDescending(person => person.Constracts.Count())
                     .Take(100).ToList();
 
             // 2. zobrazit view
             return View(top100);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            // 1. ziskat data
+            var person = _context.Persons.Find(id);
+
+            // 2. zobrazit view
+            return View(person);
         }
     }
 }
