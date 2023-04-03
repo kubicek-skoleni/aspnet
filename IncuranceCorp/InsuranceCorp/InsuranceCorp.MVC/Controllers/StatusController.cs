bca04300a58eff927538bc29aa825281d6ab7171
kubicek-skoleni <kubicek.brno@gmail.com>
2023-04-03 15:00:05 +0200

    injectnuti InsCorpDbContext do StatusController

--- IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
+++ IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsuranceCorp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InsuranceCorp.MVC.Controllers
 {
     public class StatusController : Controller
     {
+        private readonly InsCorpDbContext _context;
+
+        /// <summary>
+        /// konstruktor
+        /// </summary>
+        public StatusController(InsCorpDbContext context) 
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
