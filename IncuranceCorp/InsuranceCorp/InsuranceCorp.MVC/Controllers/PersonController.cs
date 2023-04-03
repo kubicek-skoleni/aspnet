88af3bbb4fa2f017408d83abcd50955d210f9691
kubicek-skoleni <kubicek.brno@gmail.com>
2023-04-03 15:56:11 +0200

    PersonController Index

---
+++ IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using InsuranceCorp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceCorp.MVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly InsCorpDbContext _context;
+
+        public PersonController(InsCorpDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // 1. ziskat data
+            var top100 = _context.Persons
+                    .OrderBy(person => person.Id)
+                    .Take(100).ToList();
+
+            // 2. zobrazit view
+            return View(top100);
+        }
+    }
+}
