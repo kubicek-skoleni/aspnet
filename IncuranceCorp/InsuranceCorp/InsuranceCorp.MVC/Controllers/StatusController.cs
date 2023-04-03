64e73889ab6d45c5ba9e475ed7c7849dbce0349f
kubicek-skoleni <kubicek.brno@gmail.com>
2023-04-03 13:13:01 +0200

    /status healthceck

---
+++ IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceCorp.MVC.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
