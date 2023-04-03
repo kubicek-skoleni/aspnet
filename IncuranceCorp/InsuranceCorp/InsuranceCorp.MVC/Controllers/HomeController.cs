a0dc75b01b7516f8d529bfa45254971f6696ed6a
kubicek-skoleni <kubicek.brno@gmail.com>
2023-04-03 11:38:52 +0200

    mvc hello world

--- IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/HomeController.cs
+++ IncuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace InsuranceCorp.MVC.Controllers
             return View();
         }
 
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
